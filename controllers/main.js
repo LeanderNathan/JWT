2c47782af14b4021d18da368450c6f2e55d7ed57
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 15:04:12 +0530

    Added routes and controllers

---
+++ controllers/main.js
@@ -0,0 +1,18 @@
+const login = async (req, res) => {
+  res.send('Fake Login/Register/Signup Route')
+}
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res
+    .status(200)
+    .json({
+      msg: `Hello, John Doe`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    })
+}
+
+module.exports = {
+  login,
+  dashboard,
+}
