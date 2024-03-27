191b7e5d4b5f113122422f676f5fde5187b3430c
Leander Nathan <leander.nathan@gmail.com>
2024-03-27 12:26:59 +0530

    Added module for http status codes

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,9 +1,10 @@
+const { StatusCodes } = require('http-status-codes')
 const CustomAPIError = require('./custom-error')
 
 class BadRequest extends CustomAPIError {
   constructor(message, statusCode) {
     super(message)
-    this.statusCode = 400
+    this.statusCode = StatusCodes.BAD_REQUEST
   }
 }
 
--- errors/unauth.js
+++ errors/unauth.js
@@ -1,9 +1,10 @@
+const { StatusCodes } = require('http-status-codes')
 const CustomAPIError = require('./custom-error')
 
 class UnAuthError extends CustomAPIError {
   constructor(message, statusCode) {
     super(message)
-    this.statusCode = 401
+    this.statusCode = StatusCodes.UNAUTHORIZED
   }
 }
 
