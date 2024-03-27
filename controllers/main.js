1a8e12692b2aea962a34efedce17e8356385b624
Leander Nathan <leander.nathan@gmail.com>
2024-03-27 10:58:22 +0530

    Added input validation and JWT structure

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,25 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
 const login = async (req, res) => {
-  res.send('Fake Login/Register/Signup Route')
+  const { username, password } = req.body
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400)
+  }
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100)
-  res
-    .status(200)
-    .json({
-      msg: `Hello, John Doe`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    })
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  })
 }
 
 module.exports = {
